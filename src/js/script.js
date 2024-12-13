36351acdb44ba3fbf53459fbc822a4407dee704e
stan2590 <evan.gay@edu.univ-fcomte.fr>
2024-12-13 14:07:11 +0100

    menu

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+// Sélectionner les éléments
+const menuButton = document.querySelector('.menu-btn');
+const closeButton = document.querySelector('.close-btn');
+const menu = document.querySelector('.menu');
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (menuButton && closeButton && menu) {
+  // Ouvrir le menu avec le bouton ☰
+  menuButton.addEventListener('click', () => {
+    const isOpen = menuButton.getAttribute('aria-expanded') === 'true';
+    const isClosed = !isOpen;
+
+    // Mise à jour des attributs ARIA pour l'accessibilité
+    menuButton.setAttribute('aria-expanded', isClosed);
+    menu.setAttribute('aria-hidden', isOpen ? 'true' : 'false');
+    page.classList.toggle("noscroll", isClosed); // Désactive le scroll si le menu est ouvert
+  });
+
+  // Fermer le menu avec la croix ×
+  closeButton.addEventListener('click', () => {
+    menuButton.setAttribute('aria-expanded', 'false');
+    menu.setAttribute('aria-hidden', 'true');
+    page.classList.remove("noscroll"); // Réactive le scroll lorsque le menu est fermé
+  });
+}
+
+
+
+
+
+
+
+
